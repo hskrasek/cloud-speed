cb35980e6aa167320a45b056451160ce5125f104
Hunter Skrasek <hunterskrasek@me.com>
2025-02-17 20:48:12 -0600

    Finish information output, and implement upload requests
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 extern crate clap;
 
 mod cloudflare;
+mod stats;
 
 use crate::cloudflare::client::Client;
 use crate::cloudflare::requests::{download::Download, locations::Locations, trace::TraceRequest};
+use crate::stats::{median, quartile};
 use clap::Parser;
 use std::time::Duration;
 use tokio::time::Instant;
+use crate::cloudflare::requests::upload::Upload;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -27,10 +30,57 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let latency = measure_latency(&measurements).await;
     let jitter = measure_jitter(&measurements).await;
 
-    println!("Server Location {} ({})", location.city, trace.colo);
-    println!("Your IP {} ({})", trace.ip, trace.loc);
+    println!("Server Location: {} ({})", location.city, trace.colo);
+    println!("Your IP: {} ({})", trace.ip, trace.loc);
     println!("Latency: {} ms", latency.as_millis());
     println!("Jitter: {} ms", jitter);
+
+    let download_measurements_100kb = measure_download(&client, 1e+5 as usize, 10).await;
+    println!(
+        "100kB speed: {:.2} Mbps",
+        median(&download_measurements_100kb)
+    );
+    let download_measurements_1mb = measure_download(&client, 1e+6 as usize, 8).await;
+    println!("1MB speed: {:.2} Mbps", median(&download_measurements_1mb));
+    let download_measurements_10mb = measure_download(&client, 1e+7 as usize, 6).await;
+    println!(
+        "10MB speed: {:.2} Mbps",
+        median(&download_measurements_10mb)
+    );
+    let download_measurements_25mb = measure_download(&client, 2.5e+7 as usize, 4).await;
+    println!(
+        "25MB speed: {:.2} Mbps",
+        median(&download_measurements_25mb)
+    );
+    let download_measurements_100mb = measure_download(&client, 1e+8 as usize, 1).await;
+    println!(
+        "100MB speed: {:.2} Mbps",
+        median(&download_measurements_100mb)
+    );
+
+    let download_measurements: Vec<Duration> = vec![
+        download_measurements_100kb.as_slice(),
+        download_measurements_1mb.as_slice(),
+        download_measurements_10mb.as_slice(),
+        download_measurements_25mb.as_slice(),
+        download_measurements_100mb.as_slice(),
+    ]
+    .concat();
+    
+    println!("Download speed: {:.2} Mbps", quartile(&download_measurements, 0.9));
+    
+    let upload_measurements_10kb = measure_upload(&client, 1e+4 as usize, 10).await;
+    let upload_measurements_100kb = measure_upload(&client, 1e+5 as usize, 10).await;
+    let upload_measurements_1mb = measure_upload(&client, 1e+6 as usize, 8).await;
+    
+    let upload_measurements: Vec<Duration> = vec![
+        upload_measurements_10kb.as_slice(),
+        upload_measurements_100kb.as_slice(),
+        upload_measurements_1mb.as_slice(),
+    ]
+    .concat();
+    
+    println!("Upload speed: {:.2} Mbps", quartile(&upload_measurements, 0.9));
 
     Ok(())
 }
@@ -67,4 +117,32 @@ async fn download_measurements(client: &Client) -> Vec<Duration> {
     }
 
     measurements
+}
+
+async fn measure_download(client: &Client, bytes: usize, iterations: usize) -> Vec<Duration> {
+    let mut downloads = vec![];
+    let download_request = Download { bytes };
+
+    for _ in 0..iterations {
+        let start = Instant::now();
+        let _ = client.send(download_request).await;
+        let measurement = Instant::now().duration_since(start);
+        downloads.push(measurement);
+    }
+
+    downloads
+}
+
+async fn measure_upload(client: &Client, bytes: usize, iterations: usize) -> Vec<Duration> {
+    let mut uploads = vec![];
+    let upload_request = Upload::new(bytes);
+    
+    for _ in 0..iterations {
+        let start = Instant::now();
+        let _ = client.send(&upload_request).await;
+        let measurement = Instant::now().duration_since(start);
+        uploads.push(measurement);
+    }
+    
+    uploads
 }
---
+++ src/stats.rs
@@ -0,0 +1,49 @@
+use std::time::Duration;
+
+pub fn median(test_durations: &Vec<Duration>) -> f64 {
+    let mut test_durations_u128 = test_durations
+        .iter()
+        .map(|duration| duration.as_millis())
+        .collect::<Vec<u128>>();
+
+    test_durations_u128.sort();
+
+    let mid = test_durations_u128.len() / 2;
+
+    if test_durations_u128.len() % 2 == 0 {
+        mean(&vec![
+            test_durations_u128[mid - 1],
+            test_durations_u128[mid],
+        ])
+    } else {
+        test_durations_u128[mid] as f64
+    }
+}
+
+pub fn mean(test_durations: &Vec<u128>) -> f64 {
+    let sum = test_durations.iter().sum::<u128>();
+
+    sum as f64 / test_durations.len() as f64
+}
+
+pub fn quartile(test_durations: &Vec<Duration>, percentile: f64) -> f64 {
+    let mut test_durations_u128 = test_durations
+        .iter()
+        .map(|duration| duration.as_millis())
+        .collect::<Vec<u128>>();
+
+    test_durations_u128.sort_by(|a, b| a.cmp(b));
+
+    let pos = (test_durations_u128.len() - 1) as f64 * percentile;
+    let base = pos.floor();
+    let rest = pos - base;
+
+    if base as usize + 1 < test_durations_u128.len() {
+        return test_durations_u128[base as usize] as f64
+            + rest
+                * (test_durations_u128[base as usize + 1] as f64
+                    - test_durations_u128[base as usize] as f64);
+    }
+
+    test_durations_u128[base as usize] as f64
+}
